36f41827c8e04c034c53c0448134fe4875d70bdf
yadavnan <yadavnan55@gmail.com>
2024-11-22 12:35:34 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // This config is optional for basic setups
   server: {
-    open: true,  // Automatically open the app in browser on startup
+    open: true,  // Automatically open the app in the browser on startup
   },
+  base: 'https://github.com/yadavnan/todolist.git', // Replace with the name of your repository
 });
